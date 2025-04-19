8620c9d0a6133160b15120c31bb5a997c2ee234b
mmhachem <mmh156@mail.aub.edu>
2025-04-20 02:02:44 +0300

    fixed ui transactions

--- src/main/java/com/mmhachem/exchange/transactions/Transactions.java
+++ src/main/java/com/mmhachem/exchange/transactions/Transactions.java
@@ -1,13 +1,16 @@
 package com.mmhachem.exchange.transactions;
 
-import com.mmhachem.exchange.api.model.Transaction;
-import com.mmhachem.exchange.api.model.ExchangeService;
 import com.mmhachem.exchange.Authentication;
+import com.mmhachem.exchange.api.model.ExchangeService;
+import com.mmhachem.exchange.api.model.Transaction;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,32 +20,48 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class Transactions implements Initializable {
+
     @FXML
-    public TableColumn<Transaction, Long> lbpAmount;
-    @FXML
-    public TableColumn<Transaction, Long> usdAmount;
-    @FXML
-    public TableColumn<Transaction, String> transactionDate;
-    @FXML
-    public TableView<Transaction> tableView;
+    private VBox cardContainer;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        lbpAmount.setCellValueFactory(new PropertyValueFactory<>("lbp_amount"));
-        usdAmount.setCellValueFactory(new PropertyValueFactory<>("usd_amount"));
-        transactionDate.setCellValueFactory(new PropertyValueFactory<>("added_date"));
+        String token = "Bearer " + Authentication.getInstance().getToken();
 
-        ExchangeService.exchangeApi().getTransactions("Bearer " + Authentication.getInstance().getToken())
-                .enqueue(new Callback<List<Transaction>>() {
-                    @Override
-                    public void onResponse(Call<List<Transaction>> call, Response<List<Transaction>> response) {
-                        tableView.getItems().setAll(response.body());
-                    }
+        ExchangeService.exchangeApi().getTransactions(token).enqueue(new Callback<List<Transaction>>() {
+            @Override
+            public void onResponse(Call<List<Transaction>> call, Response<List<Transaction>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Platform.runLater(() -> populateCards(response.body()));
+                }
+            }
 
-                    @Override
-                    public void onFailure(Call<List<Transaction>> call, Throwable throwable) {
-                        System.err.println("Failed to load com.mmhachem.exchange.transactions: " + throwable.getMessage());
-                    }
-                });
+            @Override
+            public void onFailure(Call<List<Transaction>> call, Throwable throwable) {
+                System.err.println("Failed to load transactions: " + throwable.getMessage());
+            }
+        });
+    }
+
+    private void populateCards(List<Transaction> transactions) {
+        cardContainer.getChildren().clear();
+
+        for (Transaction tx : transactions) {
+            HBox card = new HBox(30);
+            card.getStyleClass().add("transaction-card");
+            card.setPadding(new Insets(15));
+
+            Label usdLabel = new Label("USD: " + tx.getUsd_amount());
+            usdLabel.getStyleClass().add("card-label");
+
+            Label lbpLabel = new Label("LBP: " + tx.getLbp_amount());
+            lbpLabel.getStyleClass().add("card-label");
+
+            Label dateLabel = new Label("Date: " + tx.getAdded_date());
+            dateLabel.getStyleClass().add("card-label");
+
+            card.getChildren().addAll(usdLabel, lbpLabel, dateLabel);
+            cardContainer.getChildren().add(card);
+        }
     }
 }
